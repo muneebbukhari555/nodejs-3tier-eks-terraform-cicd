9e5132b567963281bd3b51522d30bf0bccd0103c
Syed Muneeb Ahmad Bukhari <muneebbukhari@Syeds-MacBook-Pro.local>
2026-06-04 18:24:02 +0100

    Set Block in Helm Release fixed

--- infra/terraform/modules/addons/main.tf
+++ infra/terraform/modules/addons/main.tf
@@ -23,30 +23,17 @@ resource "helm_release" "lb_controller" {
   chart      = "aws-load-balancer-controller"
   namespace  = "kube-system"
   version    = var.lb_controller_chart_version
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-  set {
-    name  = "serviceAccount.create"
-    value = "true"
-  }
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
-  set {
-    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.lb_controller_irsa.iam_role_arn
-  }
-  set {
-    name  = "region"
-    value = var.region
-  }
-  set {
-    name  = "vpcId"
-    value = var.vpc_id
-  }
+  set = [
+    { name = "clusterName", value = var.cluster_name },
+    { name = "serviceAccount.create", value = "true" },
+    { name = "serviceAccount.name", value = "aws-load-balancer-controller" },
+    {
+      name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = module.lb_controller_irsa.iam_role_arn
+    },
+    { name = "region", value = var.region },
+    { name = "vpcId", value = var.vpc_id },
+  ]
 }
 
 module "cluster_autoscaler_irsa" {
@@ -73,22 +60,15 @@ resource "helm_release" "cluster_autoscaler" {
   chart      = "cluster-autoscaler"
   namespace  = "kube-system"
   version    = var.cluster_autoscaler_chart_version
-  set {
-    name  = "awsRegion"
-    value = var.region
-  }
-  set {
-    name  = "autoDiscovery.clusterName"
-    value = var.cluster_name
-  }
-  set {
-    name  = "rbac.serviceAccount.name"
-    value = "cluster-autoscaler"
-  }
-  set {
-    name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = module.cluster_autoscaler_irsa.iam_role_arn
-  }
+  set = [
+    { name = "awsRegion", value = var.region },
+    { name = "autoDiscovery.clusterName", value = var.cluster_name },
+    { name = "rbac.serviceAccount.name", value = "cluster-autoscaler" },
+    {
+      name  = "rbac.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = module.cluster_autoscaler_irsa.iam_role_arn
+    },
+  ]
 }
 
 resource "helm_release" "metrics_server" {
